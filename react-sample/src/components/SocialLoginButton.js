72c701f44734c7a09e50792458c1fb442f11cb27
Daniel Lin <daniel@aikon.com>
2018-12-20 15:27:23 -0800

    Add another social platform style

--- react-sample/src/components/SocialLoginButton.js
+++ react-sample/src/components/SocialLoginButton.js
@@ -2,6 +2,25 @@ import React, {Component} from 'react';
 import * as oreidHelper from '../oreidHelper';
 
 import facebookLogo from '../_images/logo-facebook.svg';
+import twitterLogo from '../_images/logo-twitter.svg';
+
+const loginButtonStyle = {
+  padding: '10px 24px 10px 12px',
+  backgroundColor: '#3E5895',
+  color: '#ffffff',
+  fontFamily: 'Roboto',
+  fontSize: '14px',
+  fontWeight: '500',
+  letterSpacing: '0.25px',
+  border: 'none',
+  borderRadius: '5px',
+  boxShadow: '0 4px 12px rgba(0,0,0,0.25)',
+}
+
+const loginLogoStyle = {
+  width: '16px',
+  marginRight: '12px'
+}
 
 class SocialLoginButton extends Component {
   constructor(props) {
@@ -23,29 +42,11 @@ class SocialLoginButton extends Component {
     }));
   }
 
-  render() {
-    const facebookLoginStyle = {
-      padding: '10px 24px 10px 12px',
-      backgroundColor: '#3E5895',
-      color: '#fff',
-      fontFamily: 'Roboto',
-      fontSize: '14px',
-      fontWeight: '500',
-      letterSpacing: '0.25px',
-      border: 'none',
-      borderRadius: '5px',
-      boxShadow: '0 4px 12px rgba(0,0,0,0.25)',
-    }
-
-    const facebookLogoStyle = {
-      width: '16px',
-      marginRight: '12px'
-    }
-
+  renderFacebook() {
     return (
       <div>
-        <button style={facebookLoginStyle} onClick={this.handleLoginClick}>
-          <img style={facebookLogoStyle} src={facebookLogo} />Log in with Facebook
+        <button style={loginButtonStyle} onClick={this.handleLoginClick}>
+          <img style={loginLogoStyle} src={facebookLogo} />Log in with Facebook
         </button>
 
         {/* // SCSS Button */}
@@ -53,6 +54,27 @@ class SocialLoginButton extends Component {
           Log in with Facebook
         </button>
       </div>
+    );
+  }
+
+  renderTwitter() {
+    return (
+      <div>
+        <button style={loginButtonStyle} onClick={this.handleLoginClick}>
+          <img style={loginLogoStyle} src={facebookLogo} />Log in with Facebook
+        </button>
+
+        {/* // SCSS Button */}
+        <button className="social-login-btn--twitter" onClick={this.handleLoginClick}>
+          Log in with Twitter
+        </button>
+      </div>
+    );
+  }
+
+  render () {
+    return (
+      this.renderFacebook()
     );
   }
 }
