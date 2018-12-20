b0d5a053d2e1a50eb0624fc1f7d258f87f9224ad
Daniel Lin <daniel@aikon.com>
2018-12-20 13:59:56 -0800

    Pull out login button into component

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -4,65 +4,22 @@ import * as oreidHelper from './oreidHelper.js';
 import './App.css';
 import facebookLogo from '../src/_images/logo-facebook.svg';
 
+import SocialLoginButton from '../src/components/SocialLoginButton';
+
 require('dotenv').config();
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      isLoggedIn: false,
-      fbId: null
-    };
-
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-  }
-
-  handleLoginClick(event) {
-    event.preventDefault();
-    const id = oreidHelper.getOreIdUrl("facebook");
-    this.setState(state => ({
-      isLoggedIn: !state.isLoggedIn,
-      fbId: id,
-    }));
+    this.state = { };
   }
 
   render() {
-    const { isLoggedIn } = this.state;
-
-    const facebookLoginStyle = {
-      padding: '10px 24px 10px 12px',
-      backgroundColor: '#3E5895',
-      color: '#fff',
-      fontFamily: 'Roboto',
-      fontSize: '14px',
-      fontWeight: '500',
-      letterSpacing: '0.25px',
-      border: 'none',
-      borderRadius: '5px',
-      boxShadow: '0 4px 12px rgba(0,0,0,0.25)',
-    }
-
-    const facebookLogoStyle = {
-      width: '16px',
-      marginRight: '12px'
-    }
-
     return (
       <div className="login-wrapper">
         <div className="login-container">
           <div className="login-buttons-wrapper">
-            {!isLoggedIn &&
-              // React inline style button
-              <button style={facebookLoginStyle} onClick={this.handleLoginClick}>
-                <img style={facebookLogoStyle} src={facebookLogo} />Log in with Facebook
-              </button>
-            }
-            {!isLoggedIn &&
-              // SCSS Button
-              <button className="social-login-btn--facebook" onClick={this.handleLoginClick}>
-                Log in with Facebook
-              </button>
-            }
+            <SocialLoginButton/>
           </div>
         </div>
       </div>
---
+++ react-sample/src/components/SocialLoginButton.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import * as oreidHelper from '../oreidHelper';
+
+import facebookLogo from '../_images/logo-facebook.svg';
+
+class SocialLoginButton extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoggedIn: false,
+      fbId: null
+    };
+
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+  }
+
+  handleLoginClick(event) {
+    event.preventDefault();
+    const id = oreidHelper.getOreIdUrl("facebook");
+    this.setState(state => ({
+      isLoggedIn: !state.isLoggedIn,
+      fbId: id,
+    }));
+  }
+
+  render() {
+    const facebookLoginStyle = {
+      padding: '10px 24px 10px 12px',
+      backgroundColor: '#3E5895',
+      color: '#fff',
+      fontFamily: 'Roboto',
+      fontSize: '14px',
+      fontWeight: '500',
+      letterSpacing: '0.25px',
+      border: 'none',
+      borderRadius: '5px',
+      boxShadow: '0 4px 12px rgba(0,0,0,0.25)',
+    }
+
+    const facebookLogoStyle = {
+      width: '16px',
+      marginRight: '12px'
+    }
+
+    return (
+      <div>
+        <button style={facebookLoginStyle} onClick={this.handleLoginClick}>
+          <img style={facebookLogoStyle} src={facebookLogo} />Log in with Facebook
+        </button>
+      </div>
+    );
+  }
+}
+
+export default SocialLoginButton;
