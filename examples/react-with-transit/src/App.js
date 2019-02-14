d64a57421691080db903763bef18a2db84802be8
Tyler Kuhn <tyler@wargoats.com>
2019-02-14 13:51:59 -0800

    use individual transit providers for each login type

--- examples/react-with-transit/src/App.js
+++ examples/react-with-transit/src/App.js
@@ -14,31 +14,73 @@ const {
   REACT_APP_BACKGROUND_COLOR:backgroundColor  // Background color shown during login flow
 } = process.env;
 
-const accessContext = initAccessContext({
+const PROVIDERS = [
+  'facebook',
+  'twitter',
+  'github',
+  'twitch',
+  'linkedin',
+  'google'
+];
+
+const NETWORK_EOS = {
+  host: 'api.eosnewyork.io',
+  port: 443,
+  protocol: 'https',
+  chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+};
+
+const NETWORK_ORE = {
+  //host: 'ore.openrights.exchange',
+  host: 'ore-staging.openrights.exchange',
+  port: 443,
+  protocol: 'https',
+  chainId: 'a6df478d5593b4efb1ea20d13ba8a3efc1364ee0bf7dbd85d8d756831c0e3256' // staging
+};
+
+const WALLET_CONFIG = {
+  appId,
+  apiKey,
+  oreIdUrl,
+  authCallback,
+  //loginType: 'facebook',
+  signCallback: authCallback, // TODO: Make unique
+  backgroundColor
+};
+
+function getProvider(loginType) {
+  return oreid({
+    ...WALLET_CONFIG,
+    loginType,
+  });
+}
+
+function getContext(loginType) {
+  return initAccessContext({
+    appName: 'OreID',
+    network: NETWORK_ORE,
+    walletProviders: [
+      getProvider(loginType)
+    ],
+  });
+}
+
+function getWallet(loginType) {
+  const context = getContext(loginType);
+  const providers = context.getWalletProviders();
+  return context.initWallet(providers[0]);
+}
+
+const accessContextEos = initAccessContext({
   appName: 'OreID',
-  network: {
-    host: 'api.eosnewyork.io',
-    port: 443,
-    protocol: 'https',
-    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
-  },
+  network: NETWORK_EOS,
   walletProviders: [
-    oreid({
-      appId,
-      apiKey,
-      oreIdUrl,
-      authCallback,
-      loginType: 'facebook',
-      signCallback: authCallback, // TODO: Make unique
-      backgroundColor
-    }),
     scatter()
   ]
 });
 
-const providers = accessContext.getWalletProviders();
-const wallet_oreid = accessContext.initWallet(providers[0]);
-const wallet_scatter = accessContext.initWallet(providers[1]);
+const providersEos = accessContextEos.getWalletProviders();
+const wallet_scatter = accessContextEos.initWallet(providersEos[0]);
 
 class App extends Component {
   constructor(props) {
@@ -49,140 +91,99 @@ class App extends Component {
       accountInfo: undefined,
     };
     this.handleLogin = this.handleLogin.bind(this);
-    this.handleLogout = this.handleLogout.bind(this);
   }
 
-async componentWillMount() {
-  console.log("State:", this.state);
-  console.log("Wallet: Oreid:", wallet_oreid);
-  console.log("Wallet: Scatter:", wallet_scatter);
+  async componentWillMount() {
+    console.log("State:", this.state);
+    console.log("Wallet: Scatter:", wallet_scatter);
 
-  console.log("Connecting to wallets...");
-  try { await wallet_oreid.connect(); } catch(error) { console.log("Failed to connect to OreId:", error) }
-  try { await wallet_scatter.connect(); } catch(error) { console.log("Failed to connect to Scatter:", error) }
-  console.log("Connected wallets: OreId:", wallet_oreid.connected, "Scatter:", wallet_scatter.connected);
-}
+    console.log("Connecting to wallets...");
+    try { await wallet_scatter.connect(); } catch(error) { console.log("Failed to connect to Scatter:", error) }
+    console.log("Connected wallets: Scatter:", wallet_scatter.connected);
+  }
 
-async handleLogin(loginType) {
-  console.log(`Logging into ${loginType}...`);
-  try {
-    if (loginType === 'scatter') {
-      if (wallet_scatter.connected) {
-        const accountInfo = await wallet_scatter.login();
-        this.setState({ accountInfo });
-        console.log("Logged in!", wallet_scatter.authenticated);
-      } else {
-        throw(new Error("Scatter not connected!"));
+  async handleLogin(loginType) {
+    console.log(`Logging into ${loginType}...`);
+    try {
+      if (loginType === 'scatter') {
+        if (wallet_scatter.connected) {
+          const accountInfo = await wallet_scatter.login();
+          this.setState({ accountInfo });
+          console.log("Logged in!", wallet_scatter.authenticated);
+        } else {
+          throw(new Error("Scatter not connected!"));
+        }
+      } else { // facebook, github, etc...
+        const wallet = getWallet(loginType);
+        //wallet.login('1pafyyzw1ujv');
+        wallet.login();
       }
-    } else {
-      if (wallet_oreid.connected) {
-        await wallet_oreid.login();
-      } else {
-        throw(new Error("OreId not connected!"));
-      }
+    } catch(error) {
+      console.log("Failed to login:", error);
     }
-  } catch(error) {
-    console.log("Failed to login:", error);
   }
-}
 
-handleLogout() {
-  console.log("Logging out...");
-  wallet_oreid.logout();
-  wallet_scatter.logout();
-  console.log("Logged out!");
-}
-
-render() {
-  return (
-    <div>
-      {!this.state.isLoggedIn &&
-        this.renderLoginButtons()
-      }
-      {this.state.isLoggedIn &&
-        this.renderUserInfo()
-      }
-    </div>
-  );
-}
-
-renderUserInfo() {
-  if (this.state.userInfo) {
-    const {accountName, email, name, picture, username} = this.state.userInfo;
+  render() {
     return (
       <div>
-        <img src={picture} style={{width:50,height:50}} alt="avatar" /><br/>
-        accountName: {accountName}<br/>
-        name: {name}<br/>
-        username: {username}<br/>
-        email: {email}<br/>
-        <button onClick={this.handleLogout}  style={{ padding: '10px', backgroundColor: '#FFFBE6', borderRadius: '5px'}}>
-          Logout
-        </button>
-      </div>
-    );
-  } else if (this.state.accountInfo) {
-    const {accountName} = this.state.accountInfo;
-    return (
-      <div>
-        accountName: {accountName}<br/>
-        <button onClick={this.handleLogout}  style={{ padding: '10px', backgroundColor: '#FFFBE6', borderRadius: '5px'}}>
-          Logout
-        </button>
+        {!this.state.isLoggedIn &&
+          this.renderLoginButtons()
+        }
+        {this.state.isLoggedIn &&
+          this.renderUserInfo()
+        }
       </div>
     );
   }
-}
 
-renderLoginButtons() {
-  return (
-    <div>
-      <LoginButton provider='scatter'
-                  buttonStyle={{width:250}}
-                  logoStyle={{marginLeft:0}}
-                  onClick={()=>this.handleLogin("scatter")}
-                  //  text='Log in with Facebook'
-      />
-      <LoginButton provider='facebook'
-                  buttonStyle={{width:250, marginTop: '24px'}}
-                  logoStyle={{marginLeft:0}}
-                  onClick={()=>this.handleLogin("facebook")}
-                  //  text='Log in with Facebook'
-      />
-      <LoginButton provider='twitter'
-                  buttonStyle={{width:250, marginTop:'24px'}}
-                  logoStyle={{marginLeft:0}}
-                  onClick={()=>this.handleLogin("twitter")}
-                  //  text='Log in with Twitter'
-      />
-      <LoginButton provider='github'
-                  buttonStyle={{width:250, marginTop:'24px'}}
-                  logoStyle={{marginLeft:0}}
-                  onClick={()=>this.handleLogin("github")}
-                  //  text='Log in with Github'
-      />
-      <LoginButton provider='twitch'
-                  buttonStyle={{width:250, marginTop:'24px'}}
-                  logoStyle={{marginLeft:0}}
-                  onClick={()=>this.handleLogin("twitch")}
-                  //  text='Log in with Twitch'
-      />
-      <LoginButton provider='linkedin'
-                  buttonStyle={{width:250, marginTop:'24px'}}
-                  logoStyle={{marginLeft:0}}
-                  onClick={()=>this.handleLogin("linkedin")}
-                  //  text='Log in with LinkedIn'
-      />
-      <LoginButton provider='google'
-                  buttonStyle={{width:250, marginTop:'24px'}}
-                  logoStyle={{marginLeft:0}}
-                  onClick={()=>this.handleLogin("google")}
-                  //  text='Log in with Google'
-      />
-    </div>
-  )
-}
+  renderUserInfo() {
+    if (this.state.userInfo) {
+      const {accountName, email, name, picture, username} = this.state.userInfo;
+      return (
+        <div>
+          <img src={picture} style={{width:50,height:50}} alt="avatar" /><br/>
+          accountName: {accountName}<br/>
+          name: {name}<br/>
+          username: {username}<br/>
+          email: {email}<br/>
+          <button onClick={this.handleLogout}  style={{ padding: '10px', backgroundColor: '#FFFBE6', borderRadius: '5px'}}>
+            Logout
+          </button>
+        </div>
+      );
+    } else if (this.state.accountInfo) {
+      const {accountName} = this.state.accountInfo;
+      return (
+        <div>
+          accountName: {accountName}<br/>
+          <button onClick={this.handleLogout}  style={{ padding: '10px', backgroundColor: '#FFFBE6', borderRadius: '5px'}}>
+            Logout
+          </button>
+        </div>
+      );
+    }
+  }
 
+  renderLoginButtons() {
+    const loginButtons = PROVIDERS.map((provider) => {
+      return <LoginButton provider={provider}
+              buttonStyle={{width:250, marginTop:'24px'}}
+              logoStyle={{marginLeft:0}}
+              onClick={()=>this.handleLogin(provider)} />
+    });
+
+    return (
+      <div>
+        <LoginButton provider='scatter'
+                    buttonStyle={{width:250}}
+                    logoStyle={{marginLeft:0}}
+                    onClick={()=>this.handleLogin("scatter")}
+                    //  text='Log in with Facebook'
+        />
+        {loginButtons}
+      </div>
+    )
+  }
 }
 
 export default App;
