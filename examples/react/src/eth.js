2d2393e4d29105545d8cda58ebb11018ac659f61
Surabhi Lodha <surabhil@usc.edu>
2020-04-24 15:04:40 -0700

    eth module to pay for gas and token transfer function

---
+++ examples/react/src/eth.js
@@ -0,0 +1,325 @@
+const Web3 = require('web3') 
+const Tx = require('ethereumjs-tx').Transaction
+const { toBuffer } = require('ethereumjs-util')
+
+export const ABI = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'Approval',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'Transfer',
+    type: 'event',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+    ],
+    name: 'allowance',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'approve',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'account',
+        type: 'address',
+      },
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'subtractedValue',
+        type: 'uint256',
+      },
+    ],
+    name: 'decreaseAllowance',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'addedValue',
+        type: 'uint256',
+      },
+    ],
+    name: 'increaseAllowance',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'mint',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'recipient',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'transfer',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'sender',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'recipient',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'transferFrom',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+]
+
+export async function getGasParams(from, web3){
+  const gasPrice = await parseInt(web3.eth.getGasPrice(), 10)
+  const gasLimit = await web3.eth.getBlock('latest').gasLimit
+  const nonce = await web3.eth.getTransactionCount(from, 'pending')
+  return { gasPrice, gasLimit, nonce }
+}
+
+export async function init(url){
+  console.log('url', url)
+  const web3 = new Web3(url)
+  return web3
+}
+
+export function signAndSerializeTransaction(rawTx, privateKey){
+  const tx = new Tx(rawTx)
+  tx.sign(privateKey)
+  const serializedTx = tx.serialize();
+  return serializedTx
+}
+
+export function sendSignedTransaction(transaction, web3){
+  return new Promise((resolve, reject) => {
+    web3.eth.sendSignedTransaction('0x' + transaction.toString('hex'))
+      .on('receipt', receipt => {
+        resolve(receipt)
+      })
+      .on('error', err => {
+        reject(err)
+      })
+    })
+}
+
+export async function transferErc20Token(from, to, value, privateKey, web3){
+  const { gasPrice, gasLimit, nonce } = await getGasParams(from, web3)
+  const rawTx = {
+    nonce,
+    gasPrice: '0x09184e72a000',
+    gasLimit: '0x2710',
+    from,
+    to,
+    contract: {
+      abi: ABI,
+      parameters: [to, value], 
+      method: 'transfer',
+    },
+  }
+
+  const transaction = signAndSerializeTransaction(rawTx, privateKey)
+  const result = await sendSignedTransaction(transaction, web3)
+  return result;
+}
+
+export async function addEthForGas(from, to, value, privateKey, web3){
+  const { gasPrice, gasLimit, nonce } = await getGasParams(from, web3)
+  const rawTx = {
+    nonce,
+    gasPrice,
+    gasLimit,
+    from,
+    to,
+    value
+  }
+  console.log('rawTx',rawTx)
+  const transaction = signAndSerializeTransaction(rawTx, privateKey)
+  console.log('transaction',transaction)
+  const result = await sendSignedTransaction(transaction, web3)
+  return result;
+}
