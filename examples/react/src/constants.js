94371211d30a8d298bac3de7089e602c782c6928
Surabhi Lodha <surabhil@usc.edu>
2020-04-28 13:47:19 -0700

    add constants file

---
+++ examples/react/src/constants.js
@@ -0,0 +1,5 @@
+export const EOS_CHAIN_NETWORK='eos_kylin'
+export const ETH_CHAIN_NETWORK='eth_ropsten'
+export const ERC20_FUNDING_AMOUNT=100
+export const ERC20_TRANSFER_AMOUNT=10
+export const ETH_TRANSFER_AMOUNT='0.00001'
