fb2a01f397ed77dee73cae20adc01b560c935da7
Surabhi Lodha <surabhil@usc.edu>
2020-05-01 12:01:42 -0700

    update constant

--- examples/react/src/constants.js
+++ examples/react/src/constants.js
@@ -2,4 +2,4 @@ export const EOS_CHAIN_NETWORK='eos_kylin'
 export const ETH_CHAIN_NETWORK='eth_ropsten'
 export const ERC20_FUNDING_AMOUNT=100
 export const ERC20_TRANSFER_AMOUNT=1
-export const ETH_TRANSFER_AMOUNT='0.0100'
+export const ETH_TRANSFER_AMOUNT='0.1000'
