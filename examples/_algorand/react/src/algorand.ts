30e14f4f236f45e0789deb71ee1e862e922a40c2
Randy Torres <torresr14@gmail.com>
2020-08-07 12:33:19 -0700

    Add multsig

--- examples/_algorand/react/src/algorand.ts
+++ examples/_algorand/react/src/algorand.ts
@@ -12,11 +12,13 @@ import {
   Models,
   ModelsAlgorand,
 } from "@open-rights-exchange/chainjs";
+import { isNullOrUndefined } from "util";
 
-const env: any = dotenv.config();
+dotenv.config();
 
-const algoApiKey = env.REACT_APP_AGLORAND_API_KEY;
-const algoFundingPrivateKey = env.REACT_APP_ALGORAND_ALGO_FUNDING_PRIVATE_KEY as string;
+const algoApiKey = process.env.REACT_APP_AGLORAND_API_KEY;
+const algoFundingPrivateKey = process.env.REACT_APP_ALGORAND_ALGO_FUNDING_PRIVATE_KEY as string;
+const multisigAccountSigningKey = process.env.REACT_APP_ALGORAND_MULTISIG_ACCOUNT;
 
 const algoMainnetEndpoints = [
   {
@@ -64,3 +66,49 @@ export async function transferAlgosToAccount(
   ]);
   return await transaction.send();
 }
+
+interface Permission {
+  accountType: string
+  chainAccount: string
+  chainNetwork: string
+  externalWalletType: string
+  permission: string
+  permissionName: string
+  privateKeyStoredExterally: boolean
+  publicKey: string
+}
+
+/**
+ * Returns the multisig metadata for a specific account if it exists
+ */
+export const getMultisigMetadata = (userInfo: any, chainAccount: string) => {
+  const chainAccountInfo = userInfo?.permissions?.find((permission: Permission) => {
+    return permission.chainAccount === chainAccount
+  })
+
+  return chainAccountInfo?.metadata?.algorandMultisig
+}
+
+/**
+ * Returns the required chainAccounts for the multsig transaction.
+ * The `keyAccount` is in the users permissions and used to create the multisig (asset) account
+ * We find the keyAccount in user.permissions and add `multisigAccountSigningKey` which is the second
+ * account that will sign. 
+ * 
+ * Which account to use for multisigAccountSigningKey MUST be decided by the developer and be one of the
+ * keys in the `addrs` field on chain. 
+ */
+export const getMultisigChainAccountsForTransaction = (userInfo: any, chainAccount: string) => {
+  const algorandMultisig = getMultisigMetadata(userInfo, chainAccount)
+
+  if (isNullOrUndefined(algorandMultisig)) {
+    return null
+  }
+
+  const keyAccount = userInfo.permissions.find((permission: Permission) => {
+    return algorandMultisig.addrs?.includes(permission?.chainAccount)
+  })
+
+  return `${keyAccount?.chainAccount},${multisigAccountSigningKey}`
+}
+
