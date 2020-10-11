881f3526137a602658d879d33e08310647368719
Tray Lewin <tray.lewin@live.com>
2020-10-10 18:39:42 -0700

    finish proxy server code

--- examples/react/advanced/react-server/server/index.js
+++ examples/react/advanced/react-server/server/index.js
@@ -1,56 +1,21 @@
-
 // Tutorial on using a proxy server with React - https://www.twilio.com/blog/react-app-with-node-js-server-proxy
 // Project Template - https://github.com/philnash/react-express-starter
 
 const express = require('express');
-const bodyParser = require('body-parser');
-// const pino = require('express-pino-logger')();
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { addAlgorandApiKeys, addOreIdApiKeys, oreidProxyMiddleware, algorandProxyMiddleware } = require('./configProxy');
 
 const app = express();
+
+// ------ Algorand API
+// proxy /algorand/xxx requests to Algorand API (purestake.io)
+app.use('/algorand', addAlgorandApiKeys, algorandProxyMiddleware())
+// ------- ORE ID API 
+// proxy all other requests to OREID_URL server
+app.use('/oreid', addOreIdApiKeys, oreidProxyMiddleware())
+
+// body-parser should be after proxy setup
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(pino);
-
-app.use('/algorand', function (req, res, next) {
-  console.log('got to algorand proxy')
-  next()
-})
-
-// ------ Algorand
-// proxy requests to Algorand API (purestake.io)
-app.use('/algorand', addAlgorandApiKeys) 
-// Appends Algorand api key to header
-function addAlgorandApiKeys(req, res, next) {
-  req.headers['X-API-Key'] = process.env.ALGORAND_API_KEY
-  next()
-}
-app.use('/algorand/mainnet', createProxyMiddleware({ target: 'http://mainnet-algorand.api.purestake.io', changeOrigin: true }))
-app.use('/algorand/testnet', createProxyMiddleware({ target: 'http://testnet-algorand.api.purestake.io', changeOrigin: true }))
-app.use('/algorand/betanet', createProxyMiddleware({ target: 'http://betanet-algorand.api.purestake.io', changeOrigin: true }))
-
-app.use('/algorand/testnet/params', getParams)
-
-
-// ------- ORE ID api 
-app.use('/', addApiKeys)
-
-// Replaces any api key variable names with the actual secret value
-function addApiKeys(req, res, next) {
-  // remove api-key, and service-key from headers
-  for (const [key, value] of Object.entries(req.headers)) {
-    const secretkey = key.toLowerCase()
-    if(secretkey === 'api-key' || secretkey  === 'service-key') {
-      delete req.headers[secretkey]
-    }
-  }
-  // inject api-key and service-key(s) to header of request
-  if(process.env.OREID_API_KEY) req.headers['api-key'] = process.env.OREID_API_KEY
-  if(process.env.OREID_SERVICE_KEY) req.headers['service-key'] = process.env.OREID_SERVICE_KEY
-  next()
-}
-
-// proxy all other requests to this server to OREID_URL
-app.use('/', createProxyMiddleware({ target: process.env.OREID_URL, changeOrigin: true }))
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
