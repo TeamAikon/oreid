60121e2bc7ec7a7a3fc9d2c237d85f349bc82ef6
Tray Lewin <tray.lewin@live.com>
2021-07-27 12:01:31 -0700

    simplify and cleanup example

--- examples/react/advanced/react-oauth-google/src/App.js
+++ examples/react/advanced/react-oauth-google/src/App.js
@@ -1,104 +1,124 @@
-import React, { useState } from 'react';
-import { GoogleLogin, useGoogleLogin, GoogleLogout, useGoogleLogout } from 'react-google-login';
-import { OreId } from 'oreid-js'
-import logo from './logo.svg';
-import { ReactComponent as OreLogo } from './logo-ore.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  GoogleLogin,
+  useGoogleLogin,
+  GoogleLogout,
+  useGoogleLogout,
+} from "react-google-login";
+import { OreId } from "oreid-js";
+import logo from "./logo-oreid.svg";
+import "./App.css";
+
+const googleOauthClientId = "571262146536-le7c8genogladg68ubqb5l0f8nijhgr5.apps.googleusercontent.com";
 
 function App() {
-  const onSuccess = (e) => {
-    setSignedIn(true)
-    setGoogleLoginResponse(e)
-  }
+  const [loggedIn, setLoggedIn] = useState({});
+  const [user, setUser] = useState({});
+  const [googleLoginResponse, setGoogleLoginResponse] = useState(null);
+
+  useEffect(() => {
+    fetchOreIdUser();
+  }, []);
+
+  const onSuccess = async (e) => {
+    setLoggedIn(true);
+    fetchOreIdUser().then(() => {
+      const googleUser = {...e?.profileObj, idToken: e?.tokenId }
+      setGoogleLoginResponse(googleUser)
+      loginToOreId(googleUser.idToken);
+    });
+  };
 
   const onLogoutSuccess = (e) => {
-    setSignedIn(false)
-    setGoogleLoginResponse(null)
-  }
+    setLoggedIn(false);
+    setGoogleLoginResponse(null);
+    oreId.logout(); // clears ORE ID accessToken
+  };
 
   const onFailure = (e) => {
-    console.error(e)
-  }
+    console.error(e);
+  };
 
-  const userAuthCallbackUrl = `${window.location.origin}/oreid/authcallback`
+  const googleOauthProps = {
+    clientId: googleOauthClientId,
+    onFailure,
+  };
+
+  const { loaded: canLogin, isSignedIn: googleSignedIn } = useGoogleLogin({...googleOauthProps, onSuccess});
+  const { loaded: canLogout } = useGoogleLogout({ ...googleOauthProps, onLogoutSuccess });
+
+  /** Initialize Ore Id */
+  const oreId = new OreId({
+    appName: "ORE ID - Google OAuth Sample App",
+    appId: "demo_0097ed83e0a54e679ca46d082ee0e33a",
+    oreIdUrl: "http://localhost:8080",
+  });
 
   /** Load the user from local storage - user info is automatically saved to local storage by oreId.getUserInfoFromApi() */
   const fetchOreIdUser = async () => {
     let userInfo = (await oreId.getUser()) || {};
-    if( userInfo.accountName ) {
-      delete userInfo.permissions
-      window.alert(`Your ORE ID: ${JSON.stringify(userInfo, null, 2)}`)
-      return userInfo  
+    if (userInfo?.accountName) {
+      if (userInfo.accountName) setUser({ ...userInfo });
+      setLoggedIn(true);
     }
-    window.alert('Your ORE ID was not found')
-    return null
-  }
+  };
 
-  const oreId = new OreId({
-    appName: 'ORE ID - Google OAuth Sample App',
-    appId: process.env.REACT_APP_OREID_APP_ID,
-    oreIdUrl: process.env.REACT_APP_OREID_URL,
-    authCallbackUrl: userAuthCallbackUrl
-  });
+  //** Convert Google IdToken to ORE ID accessToken */
+  const loginToOreId = async (idToken) => {
+    console.log("got to handleOreIdLogin");
+    oreId.login({ idToken }).then(({ accessToken }) => {
+      oreId.accessToken = accessToken // saves accessToken in local storage
+      fetchOreIdUser();
+    });
+  };
 
+  const renderLoggedIn = () => {
+    const { accountName, email, name, picture, username } = user;
+    return (
+      <div>
+        <div className="google-logout-button">
+          <GoogleLogout { ...{...googleOauthProps, onLogoutSuccess} } />
+        </div>
+        <div style={{ marginTop: 40 }}>
+          <h4>User Info</h4>
+          <img src={picture} />
+          <br />
+          OreId account: {accountName}
+          <br />
+          name: {name}
+          <br />
+          username: {username}
+          <br />
+          email: {email}
+          <br />
+        </div>
+      </div>
+    );
+  };
 
-  const handleOreIdLogin = async () => {
-    const idToken = googleLoginResponse.tokenId;
-    let { accessToken } = await oreId.login({ idToken });
-    console.log('accessToken', accessToken)
-    await fetchOreIdUser()
-  }
-  const googleAPIprops = {
-    clientId: process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID,
-    onFailure
-  }
-
-  const googleLoginProps = {
-    ...googleAPIprops,
-    onSuccess
-  }
-
-  const { loaded: canLogin, isSignedIn } = useGoogleLogin(googleLoginProps)
-
-  const googleLogoutProps = {
-    ...googleAPIprops,
-    onLogoutSuccess
-  }
-
-  const { loaded: canLogout } = useGoogleLogout(googleLogoutProps)
-
-  const [signedIn, setSignedIn] = useState(isSignedIn)
-  const [googleLoginResponse, setGoogleLoginResponse] = useState(null)
+  const renderLoggedOut = () => {
+    return (
+      <div>
+        {!loggedIn && canLogin && (
+          <div className="google-login-button">
+            <GoogleLogin { ...{ ...googleOauthProps, onSuccess }} />
+          </div>
+        )}
+      </div>
+    );
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="ORE ID" />
+        {loggedIn ? (
+          <div>{renderLoggedIn()} </div>
+        ) : (
+          <div>{renderLoggedOut()} </div>
+        )}
+        <section className="App-section"></section>
       </header>
-      <section className="App-section" >
-          {!signedIn && canLogin &&
-            <div className="google-login-button">
-              <GoogleLogin {...googleLoginProps} />
-            </div>
-          }
-          {signedIn && (
-            <>
-              <div className="oreid-login-button">
-                <button onClick={handleOreIdLogin}>
-                  <div>
-                    <OreLogo />
-                  </div>
-                  <span>Login to ORE ID </span>
-                </button>
-              </div>
-              {canLogout && 
-                <div className="google-logout-button">
-                  <GoogleLogout {...googleLogoutProps} />
-                </div>
-              }
-            </>
-          )}
-      </section>
     </div>
   );
 }
