8c6e6328514eeed8fc6ab0d1f3cc309d9fdf6ce7
Abdul R. Tayyab <i@xn--rtman-ewa.com>
2022-02-07 15:55:16 +0500

    fix typo

--- examples/react/tutorial/step3-web-widget/src/App.js
+++ examples/react/tutorial/step3-web-widget/src/App.js
@@ -209,7 +209,7 @@ export default function App() {
   const composeWidgetOptionsForAction = (action, args) => {
     switch (action) {
       case DappAction.Sign:
-        if (!args?.selectedChainAccountPermission) {
+        if (!args?.chainAccountPermission) {
           setLogs({ [Severity.Error]: "Please select a Permission to use for Action" });
           return;
         }
