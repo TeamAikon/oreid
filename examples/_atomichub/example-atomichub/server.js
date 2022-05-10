ae77e8b4c035f55c48cae7fb82d842a142ef8330
Tray Lewin <tray.lewin@live.com>
2022-05-10 07:51:31 -0700

    add files for server deployment

---
+++ examples/_atomichub/example-atomichub/server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+
+const PORT = 8080;
+
+try {
+  const app = express();
+
+  // Hosts Frontend - Static files built with npm run build
+  app.use('/', express.static(`${__dirname}/build`));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(`${__dirname}/build/index.html`));
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Service is now running on {server}:${PORT}`);
+  });
+} catch (error) {
+  console.log(error);
+}
